30fa0551483bb86b9c2c0e5cbcbaf3a10398323d
Charles D'Angelo <c.g.dangelo@gmail.com>
2014-01-14 21:59:06 -0500

    Remove trailing whitespace
    ...which is apparently a damn switch in DartEditor. Fine.

--- web/src/game.dart
+++ web/src/game.dart
@@ -3,35 +3,35 @@ part of darts;
 class Game
 {
   static const int CROSSHAIR_SIZE = 30;
-  
+
   CanvasElement gameBoard;
   CanvasRenderingContext2D ctx;
-  
+
   ImageElement boardTexture;
   ImageElement crosshairTexture;
-  
+
   Game(CanvasElement gameBoardElement)
   {
     this.gameBoard = gameBoardElement;
     this.ctx = this.gameBoard.getContext('2d');
   }
-  
+
   void init()
   {
     this.loadTextures();
     this.bindEvents();
   }
-  
+
   void loadTextures()
   {
     this.crosshairTexture = new ImageElement(src: 'textures/cross-01.png');
   }
-  
+
   void bindEvents()
   {
     this.gameBoard.onMouseMove.listen((event) => this.handleMouseMove(event));
   }
-  
+
   void handleMouseMove(MouseEvent event)
   {
     ctx.clearRect(0, 0, 600, 600);
